a86580f62a22e6be0e52763cd232f38752bd693d
Zackariah Grogan <ZackariahGrogan@gmail.com>
2017-09-20 13:48:05 -0700

    Update client.py

--- client.py
+++ client.py
@@ -1,21 +1,24 @@
 import socket
 import os
 
-def connect
+
+
+def Connection
 soc = socket.socket()
 host = socket.gethostname()
 port 1000
 soc.bind((host, port))
 fts = open('File.txt','w')
 soc.listen(5)
-
-
-
-
-
-
-
-
+while True:
+  c, ip=soc.accept()
+  print'Receiving connection from', ip
+  rec = c.recv(1000)
+  While(rec)
+    print"Receiving File from", ip
+    f.write(rec)
+    rec = c.recv(1000)
+  c.close()
 
 
 def Info
