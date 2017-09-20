0bf5daed5f29da29c53951d149cf68bc1411fdac
Zackariah Grogan <ZackariahGrogan@gmail.com>
2017-09-20 13:12:33 -0700

    Update and rename Client.py to client.py

---
+++ client.py
@@ -0,0 +1,28 @@
+import socket
+import os
+
+def connect
+soc = socket.socket()
+host = socket.gethostname()
+port 1000
+soc.bind((host, port))
+fts = open('File.txt','w')
+soc.listen(5)
+
+
+
+
+
+
+
+
+
+
+def Info
+with with io.FileIO*(str(Ln), "w"):
+        FN = input('Enter your first name:  ')
+        LN = input('Enter your first name:  ')
+        AD = input('Enter your first name:  ')
+        UN = input('Enter your first name:  ')
+        PS = input('Enter Your Password:    ')
+    out.write('{}\n{}\n{}\n{}\n'.format(LN,AD,UN,PS))
