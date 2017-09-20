4487a90ce3552a3168204e86d9fa21c95eea0aba
Zackariah Grogan <ZackariahGrogan@gmail.com>
2017-09-20 11:16:50 -0700

    Update and rename Python.py to server.py

---
+++ server.py
@@ -0,0 +1,15 @@
+import socket
+import os
+
+soc = socket.socket()
+host = socket.gethostname()
+port 1000
+soc.bind((host, port))
+fts = open('File.txt','w')
+soc.listen(5)
+while True:
+  c, ip=soc.accept()
+  print'Receiving connection from', ip
+  rec = c.recv(1000)
+  While(rec)
+  print"Receiving File from", ip
