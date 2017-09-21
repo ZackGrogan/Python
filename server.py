52020625318069b762f27126ed1a19fb82126ffe
Zackariah Grogan <ZackariahGrogan@gmail.com>
2017-09-21 14:08:20 -0700

    Update server.py

--- server.py
+++ server.py
@@ -3,16 +3,16 @@ import os
 
 soc = socket.socket()
 host = socket.gethostname()
-port 1000
+port = 1000
 soc.bind((host, port))
 fts = open('File.txt','w')
 soc.listen(5)
 while True:
-  c, ip=soc.accept()
-  print'Receiving connection from', ip
-  rec = c.recv(1000)
-  While(rec)
-    print"Receiving File from", ip
-    f.write(rec)
-    rec = c.recv(1000)
-  c.close()
+    c, ip=soc.accept()
+print'Receiving connection from', ip
+rec = c.recv(1000)
+While(rec)
+print"Receiving File from", ip
+f.write(rec)
+rec = c.recv(1000)
+c.close()
