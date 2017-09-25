55c2a2467e53817435257faeffc96369a72a58a1
Zackariah Grogan <ZackariahGrogan@gmail.com>
2017-09-25 11:11:07 -0700

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,13 @@
 import socket
 import os
 
+with with io.FileIO*(str(Ln), "w"):
+        FN = input('Enter your first name:  ')
+        LN = input('Enter your first name:  ')
+        AD = input('Enter your first name:  ')
+        UN = input('Enter your first name:  ')
+        PS = input('Enter Your Password:    ')
+    out.write('{}\n{}\n{}\n{}\n'.format(LN,AD,UN,PS))
 
 #this is the server.py socket code i need to change it so it will send rather then recieve
 soc = socket.socket()
@@ -20,11 +27,3 @@ while True:
   c.close()
 
 
-def Info
-with with io.FileIO*(str(Ln), "w"):
-        FN = input('Enter your first name:  ')
-        LN = input('Enter your first name:  ')
-        AD = input('Enter your first name:  ')
-        UN = input('Enter your first name:  ')
-        PS = input('Enter Your Password:    ')
-    out.write('{}\n{}\n{}\n{}\n'.format(LN,AD,UN,PS))
